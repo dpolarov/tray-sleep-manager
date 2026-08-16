12c99bce0712299562fa525544409c3e2fcf908d
Dmitriy <65167625+dpolarov@users.noreply.github.com>
2026-08-16 14:20:53 +0200

    Make standalone script target main project explicitly

--- build-standalone.cmd
+++ build-standalone.cmd
@@ -6,7 +6,7 @@ echo Не требует .NET Runtime
 echo ========================================
 echo.
 
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true
+dotnet publish SleepMngr.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true
 
 if %ERRORLEVEL% EQU 0 (
     echo.
